38ce24967c5f28c02863c804f3f14bb36ea6f767
shahnCM <mahir.fine@hotmail.com>
2020-08-02 10:21:28 +0600

    added functions to image module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,19 +7,33 @@ defmodule Identicon do
     input
       |> hash_input
         |> pick_color
+          |> build_grid
   end
 
-  def pick_color(image) do
+  def pick_color(%Identicon.Image{ hex: [r, g, b | _tail] } = image) do
+    # Take image as an input
     # %Identicon.Image{hex: hex_list} = image
     # [r, g, b | _tail] = hex_list
-    %Identicon.Image{ hex: [r, g, b | _tail] } = image
-    [r, g, b]
+    Identicon.Image.update_with_color(r, g, b, image)
   end
 
   def hash_input(input) do
     :crypto.hash(:md5, input)
       |> :binary.bin_to_list
-        |> Identicon.Image.construct_image  
+        |> Identicon.Image.construct  
+  end
+
+  def build_grid(%Identicon.Image{ hex: hex_list } = image) do
+    hex_list
+      |> Enum.chunk_every(3, 3, :discard)
+        |> Enum.map(&mirror_row/1)
+          |> List.flatten
+            |> Enum.with_index
+              |> Identicon.Image.update_with_grid(image)
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first] 
   end
 
 end
--- lib/image.ex
+++ lib/image.ex
@@ -1,7 +1,16 @@
 defmodule Identicon.Image do
-    defstruct hex: nil
+    defstruct hex: nil, color: nil, grid: nil
 
-    def construct_image(hex_list) do
+    def construct(hex_list) do
         %Identicon.Image{hex: hex_list}
     end
+
+    def update_with_color(r, g, b, image) do
+        %Identicon.Image{image | color: {r, g, b}}
+    end
+
+    def update_with_grid(grid, image) do
+        %Identicon.Image{image | grid: grid}
+    end
+
 end
